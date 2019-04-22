f28d01492fd30dbadfa8590294b311a1158d2d41
Matt <mmhogg@icloud.com>
2019-04-22 17:48:02 -0500

    card changes

--- frontend/app/components/Group18Card.vue
+++ frontend/app/components/Group18Card.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="row">
       <div class="col col-6">
-        <dynamic-line-chart source="topContributors"
+        <dynamic-line-chart source="totalCommitters"
                     title="Top Authors By Commit"
                     cite-url=""
                     cite-text="Authors by Commit">
@@ -18,11 +18,10 @@
       </div>
 
       <div class="col col-6">
-        <dynamic-line-chart source="openness"
+        <dynamic-line-chart source="totalCommitters"
                     title="Repo Openness"
                     cite-url=""
                     cite-text="Repo Openness"
-                    :data="values['openness']"
                     >
         </dynamic-line-chart>
       </div>
@@ -52,33 +51,4 @@ module.exports = {
     DualAxisContributions
   }
 }
-
-created () {
-      //THIS BLOCK EXECUTES WHENEVER THIS CARD FILE GETS RENDERED,
-      // IF YOU WANT TO CALL YOUR ENDPOINT IN YOUR CARD FILE,
-      // THIS IS WHERE/HOW YOU SHOULD DO IT:
-
-      let repo = window.AugurAPI.Repo({ githubURL: this.repo })
-      repo[this.source]().then((data) => {
-        // IF YOU ARE CREATING YOUR OWN CHART FILE, SET THIS.VALUES LIKE THIS
-        //this.values['endpoint_name'] = data
-
-        // IF YOU ARE USING THE DYNAMIC LINE CHART, SET IT LIKE THIS
-        this.values['openness'][this.repo]['openness']
-        // this is so that 'this.values['endpoint_name'] = ' something with the following format (this is how DynamicLineChart.vue wants the data to be formatted):
-            /*
-            {
-            	repo_name: {
-            		endpoint_name: {
-            			[
-            				{data}, {data}, {data}
-            			]
-            		}
-            	}
-            }
-            */
-      })
-      //FINISH CALLING ENDPOINT
-  }
-
 </script>
