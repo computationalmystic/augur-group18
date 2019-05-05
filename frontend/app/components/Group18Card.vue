bd3426b745077a766382e83137ebb6533101b8ae
Matt <mmhogg@icloud.com>
2019-05-05 14:34:01 -0500

    Color coding on the archive repo UI

--- frontend/app/components/Group18Card.vue
+++ frontend/app/components/Group18Card.vue
@@ -22,7 +22,7 @@
         </dynamic-line-chart>
       </div>
       <div class="col col-12">
-        <h3>Days until this Repo is considered inactive:  100</h3>
+        <h3 style="text-align: center" id = "daysLeft">Days until this Repo is considered inactive:  100</h3>
       </div>
     </div>
 
@@ -50,4 +50,31 @@ module.exports = {
     TopAuthors
   }
 }
+
+var countDownDate = new Date("Jun 5, 2019 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("daysLeft").innerHTML = days + "d ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("daysLeft").innerHTML = "INACTIVE";
+  }
+}, 1000);
 </script>
